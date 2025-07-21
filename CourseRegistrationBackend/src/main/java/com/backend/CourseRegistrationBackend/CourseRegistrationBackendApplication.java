5d201a15470b9460e95f1c203adace20ebe56be4
Dineshika <99dinuraja@gmail.com>
2025-07-21 15:54:42 +0530

    setup springBoot project backend and connect with DB

---
+++ CourseRegistrationBackend/src/main/java/com/backend/CourseRegistrationBackend/CourseRegistrationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.CourseRegistrationBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegistrationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegistrationBackendApplication.class, args);
+	}
+
+}
