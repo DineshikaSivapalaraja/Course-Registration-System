942b1000bbc8441bc8daace8d2bd5ecbaee5242f
Dineshika <99dinuraja@gmail.com>
2025-07-23 13:43:34 +0530

    get enrolled students in frontend from backend

--- CourseRegistrationFrontend/myscript.js
+++ CourseRegistrationFrontend/myscript.js
@@ -17,3 +17,21 @@ function showCourses(){
         }))
     });
 }
+
+function enrolledStudents(){
+    fetch("http://localhost:8080/courses/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+        const dataTable = document.getElementById("enrolledTable");
+
+        students.forEach((student => {
+            var row = `<tr>
+            <td>${student.name}</td>
+            <td>${student.emailId}</td>
+            <td>${student.courseName}</td>
+            </tr>`
+
+            dataTable.innerHTML+=row;
+        }))
+    });
+}
